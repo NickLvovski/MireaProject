1790580eb6aa04bdfc83ec157c39bb6fd6e64cc7
NickLvovski <nicklvovski@gmail.com>
2023-05-27 09:39:27 +0300

    lesson3-4

---
+++ app/src/main/java/ru/mirea/filatov/mireaproject/ui/browser/BrowserFragment.java
@@ -0,0 +1,67 @@
+package ru.mirea.filatov.mireaproject.ui.browser;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import ru.filatov.mireaproject.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link BrowserFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class BrowserFragment extends Fragment {
+
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    private String mParam1;
+    private String mParam2;
+
+    private WebView webView;
+
+    public BrowserFragment() {
+
+    }
+    public static BrowserFragment newInstance(String param1, String param2) {
+        BrowserFragment fragment = new BrowserFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_brouser, container, false);
+        webView = (WebView) view.findViewById(R.id.webView);
+        webView.loadUrl("https://ya.ru/");
+
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+        webView.setWebViewClient(new WebViewClient());
+        return view;
+    }
+}
